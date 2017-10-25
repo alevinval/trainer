c4cf5e6ba3680ee03287e04ff142d29270984287
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2017-10-25 22:21:42 +0200

    provider/fs: update test to start covering OpenFile

--- provider_fs_test.go
+++ provider_fs_test.go
@@ -1,23 +1,53 @@
 package trainer
 
 import (
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetFileExt(t *testing.T) {
+func createTmpFile(t *testing.T, path string, data []byte) {
+	err := ioutil.WriteFile(path, data, 0644)
+	if err != nil {
+		t.Fatalf("error writing file: %s", err)
+	}
+}
+
+func TestOpenFile(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "trainer-activities-test")
+	if err != nil {
+		t.Fatalf("error creating temporary directory: %s", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
 	for _, test := range []struct {
-		input    string
-		expected fileExt
+		fileName      string
+		data          string
+		err           error
+		dataPointsLen int
 	}{
-		{"some.gpx", extGpx},
-		{"file.tmp.gpx", extGpx},
-		{"file.gpx.tmp", extUnknown},
-		{"activity.txt", extUnknown},
-		{"", extUnknown},
+		{"wrong-extension-1", "", ErrUnknownExtension, 0},
+		{"wrong-extension-2.tmp", "", ErrUnknownExtension, 0},
+		{"wrong-extension-3.gpx.txt", "", ErrUnknownExtension, 0},
+
+		{"right-extension-no-data.gpx", "", io.EOF, 0},
+		{"right-extension-invalid-data.gpx", "invalid blob", io.EOF, 0},
+
+		{"right-extension-valid-data.gpx", "<xml></xml>", nil, 0},
 	} {
-		actual := getFileExt(test.input)
-		assert.Equal(t, test.expected, actual)
+		filePath := path.Join(tmpDir, test.fileName)
+		createTmpFile(t, filePath, []byte(test.data))
+
+		activity, err := OpenFile(filePath)
+		if !assert.Equal(t, test.err, err) {
+			return
+		}
+		if err == nil {
+			assert.Equal(t, test.dataPointsLen, len(activity.DataPoints()))
+		}
 	}
 }
