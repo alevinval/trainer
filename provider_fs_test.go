902a85053bb4ad15eec8209dc0e2c35336c0e6c4
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2017-11-06 19:06:45 +0100

    provider/fs: add an extra test to parse trackpoints

--- provider_fs_test.go
+++ provider_fs_test.go
@@ -1,10 +1,12 @@
 package trainer
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,6 +17,42 @@ func createTmpFile(t *testing.T, path string, data []byte) {
 	if err != nil {
 		t.Fatalf("error writing file: %s", err)
 	}
+}
+
+func createGpxActivityAsText() string {
+	trackpoint := func(lat, lon float64) string {
+		return fmt.Sprintf(`<trkpt lat="%f" lon="%f">
+		<ele>%f</ele>
+		<time>2017-06-19T16:49:35.000Z</time>
+		<extensions>
+			<ns3:TrackPointExtension>
+				<ns3:hr>94</ns3:hr>
+				<ns3:cad>85</ns3:cad>
+			</ns3:TrackPointExtension>
+		</extensions>
+		</trkpt>`, lat, lon, 100.0)
+	}
+
+	trackpoints := []string{
+		trackpoint(1.0, 1.0),
+		trackpoint(2.0, 2.0),
+		trackpoint(3.0, 3.0),
+	}
+	trackpointsStr := strings.Join(trackpoints, "")
+
+	return fmt.Sprintf(`<gpx creator="Garmin Connect" version="1.1"
+	xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/11.xsd"
+	xmlns="http://www.topografix.com/GPX/1/1"
+	xmlns:ns3="http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
+	xmlns:ns2="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<trk>
+		<name>Some activity name</name>
+		<type>running</type>
+		<trkseg>
+			%s
+		</trkseg>
+	</trk>
+	</gpx>`, trackpointsStr)
 }
 
 func TestOpenFile(t *testing.T) {
@@ -38,6 +76,8 @@ func TestOpenFile(t *testing.T) {
 		{"right-extension-invalid-data.gpx", "invalid blob", io.EOF, 0},
 
 		{"right-extension-valid-data.gpx", "<xml></xml>", nil, 0},
+
+		{"right-file-1.gpx", createGpxActivityAsText(), nil, 3},
 	} {
 		filePath := path.Join(tmpDir, test.fileName)
 		createTmpFile(t, filePath, []byte(test.data))
