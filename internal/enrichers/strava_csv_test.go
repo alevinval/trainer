c1e8f84837fe94e0d47deb2674e962934e09653a
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2018-07-26 18:47:09 +0200

    enricher/strava: test ignores non-files, test csv parse failures

--- internal/enrichers/strava_csv_test.go
+++ internal/enrichers/strava_csv_test.go
@@ -1,6 +1,8 @@
 package enrichers
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/alevinval/trainer/internal/trainer"
@@ -8,9 +10,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func createTmpFile(t *testing.T, path string, data []byte) {
+	err := ioutil.WriteFile(path, data, 0644)
+	if err != nil {
+		t.Fatalf("error writing file: %s", err)
+	}
+}
+
 func TestStravaCsvEnrichFailOpen(t *testing.T) {
 	_, err := NewStravaCsvEnricher("testdata/missing-file.csv")
 	assert.NotNil(t, err)
+}
+
+func TestStravaCsvEnrichFailCsvParse(t *testing.T) {
+	defer os.RemoveAll("enrich.csv")
+	createTmpFile(t, "enrich.csv", []byte("a\n,,\n"))
+
+	_, err := NewStravaCsvEnricher("enrich.csv")
+	require.NotNil(t, err)
+
+	assert.Equal(t, "record on line 2: wrong number of fields", err.Error())
 }
 
 func TestStravaCsvEnrichActivity(t *testing.T) {
@@ -29,4 +48,22 @@ func TestStravaCsvEnrichActivity(t *testing.T) {
 	enricher.Enrich(a)
 
 	assert.Equal(t, "Lunch Run", a.Metadata().Name)
+}
+
+func TestStravaCsvEnrichIgnoresNonFileActivities(t *testing.T) {
+	enricher, err := NewStravaCsvEnricher("testdata/strava_activities.csv")
+	require.Nil(t, err)
+
+	a := &trainer.Activity{}
+	a.SetMetadata(&trainer.Metadata{
+		Name: "",
+		DataSource: trainer.DataSource{
+			Type: trainer.DataSourceType("not-a-file"),
+			Name: "783319746.fit.gz",
+		},
+	})
+
+	enricher.Enrich(a)
+
+	assert.Equal(t, "", a.Metadata().Name)
 }
