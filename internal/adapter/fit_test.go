46caa2c16dc85e7f008c45b5eb1a0bd8a350ce25
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2018-07-28 20:54:34 +0200

    gpx/fit: cover with test

---
+++ internal/adapter/fit_test.go
@@ -0,0 +1,42 @@
+package adapter
+
+import (
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/alevinval/trainer/internal/trainer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadFit(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/sample.fit")
+	require.Nil(t, err)
+
+	f, err := Fit(data)
+	require.Nil(t, err)
+
+	assert.Equal(t, "", f.Metadata().DataSource.Name)
+	assert.Equal(t, trainer.DataSourceType(""), f.Metadata().DataSource.Type)
+	assert.Equal(t, "", f.Metadata().Name)
+	assert.Equal(t, "2015-01-20 14:04:05 +0000 UTC", f.Metadata().Time.String())
+	assert.Equal(t, 260, len(f.DataPoints()))
+
+	// Compare first data point
+	expectedTime, _ := time.Parse("2006-01-02T15:04:05.000Z", "2015-01-20T13:34:38.000Z")
+	expected := &trainer.DataPoint{
+		Time: expectedTime,
+		Coords: trainer.Point{
+			Lat:       25.05563087761402,
+			Lon:       121.62827912718058,
+			Elevation: 40.200000000000045,
+		},
+		Hr:    255,
+		Cad:   162,
+		Speed: 2.69334129650378,
+		Perf:  1.7110638824847544,
+		N:     1,
+	}
+	assert.Equal(t, expected, f.DataPoints()[50])
+}
--- internal/adapter/gpx_test.go
+++ internal/adapter/gpx_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/alevinval/trainer/internal/trainer"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -22,21 +21,22 @@ func TestReadGpx(t *testing.T) {
 	assert.Equal(t, trainer.DataSourceType(""), g.Metadata().DataSource.Type)
 	assert.Equal(t, "Some activity name", g.Metadata().Name)
 	assert.Equal(t, "2015-01-20 13:26:30 +0000 UTC", g.Metadata().Time.String())
-
 	assert.Equal(t, 3, len(g.DataPoints()))
 
 	// Compare first data point
-	expectedTime, _ := time.Parse("2006-01-02T15:04:05.000Z", "2017-06-19T16:49:35.000Z")
+	expectedTime, _ := time.Parse("2006-01-02T15:04:05.000Z", "2017-06-19T16:49:40.000Z")
 	expected := &trainer.DataPoint{
 		Time: expectedTime,
 		Coords: trainer.Point{
-			Lat:       1,
-			Lon:       1,
+			Lat:       1.000001,
+			Lon:       1.000001,
 			Elevation: 100,
 		},
-		Hr:  94,
-		Cad: 170,
-		N:   1,
+		Hr:    94,
+		Cad:   170,
+		Speed: 0.03148350908743588,
+		Perf:  0.05693826111557553,
+		N:     1,
 	}
-	assert.Equal(t, expected, g.DataPoints()[0])
+	assert.Equal(t, expected, g.DataPoints()[1])
 }
